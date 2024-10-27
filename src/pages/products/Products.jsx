a43702ef6a99ec189be36dc8abe0f907fea25a7f
santokhan <santokhan1999@gmail.com>
2024-10-27 17:40:33 +0600

    stop unnecessary re-render

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -223,7 +223,7 @@ const Products = () => {
                                     await request(`products/${id}/activate`, {
                                       method: "PATCH",
                                     });
-                                    await fetchProducts();
+                                    // await fetchProducts(); // We are not changing order with updated_at that is why does not require re render
                                   } catch (error) {
                                     console.error(error);
                                   }
@@ -243,7 +243,7 @@ const Products = () => {
                                     await request(`products/${id}/deactivate`, {
                                       method: "PATCH",
                                     });
-                                    await fetchProducts();
+                                    // await fetchProducts(); // We are not changing order with updated_at that is why does not require re render
                                   } catch (error) {
                                     console.error(error);
                                   }
