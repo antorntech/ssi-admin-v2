856fbc91f61cfe26d91355900a1851542f1b7cb9
santokhan <santokhan1999@gmail.com>
2024-10-30 04:45:11 +0600

    update date

--- src/utils/date.js
+++ src/utils/date.js
@@ -2,10 +2,10 @@ function formatDate(str = "") {
   if (!str) return "";
 
   const date = new Date(str);
-  
+
   if (isNaN(date.getTime())) return "Invalid date";
 
-  return date.toLocaleString("bn-BD");
+  return date.toLocaleString("en-BD");
 }
 
 export { formatDate };
