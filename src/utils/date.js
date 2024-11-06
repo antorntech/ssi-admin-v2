8e7dde769690ad70045d951f781f8922619ab793
santokhan <santokhan1999@gmail.com>
2024-11-06 09:26:49 +0600

    update date f ormatter

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,11 +1,16 @@
 function formatDate(str = "") {
   if (!str) return "";
 
-  const date = new Date(str);
+  const date = new Date(str + "Z");
 
   if (isNaN(date.getTime())) return "Invalid date";
 
-  return date.toLocaleString("en-BD", { timeZone: "Asia/Dhaka", hour12: true });
+  // "Asia/Kolkata"
+  // "Asia/Dhaka"
+  return date.toLocaleString("en-BD", {
+    timeZone: "Asia/Dhaka",
+    hour12: true,
+  });
 }
 
 export { formatDate };
