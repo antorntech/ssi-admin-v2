e0ae177df10b2bb698dab708688dad5bac30ec9a
santokhan <santokhan1999@gmail.com>
2024-11-21 22:31:32 +0600

    add new unit

--- src/pages/products/AddProduct.jsx
+++ src/pages/products/AddProduct.jsx
@@ -211,7 +211,7 @@ const AddProduct = () => {
                   required
                 >
                   <option value="" disabled></option>
-                  {["gm", "kg", "ml"].map((unit) => (
+                  {["gm", "kg", "ml", "pieces"].map((unit) => (
                     <option key={unit} value={unit}>
                       {unit}
                     </option>
